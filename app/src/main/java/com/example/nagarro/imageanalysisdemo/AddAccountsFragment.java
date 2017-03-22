263c7fd9fa4f8ef6854d8605530d182e89798dfa
Vishal Dwivedi <vishaldwivedi13@gmail.com>
2017-03-22 11:29:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nagarro/imageanalysisdemo/AddAccountsFragment.java
@@ -0,0 +1,22 @@
+package com.example.nagarro.imageanalysisdemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AddAccountsFragment extends Fragment {
+
+    public AddAccountsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_accounts, container, false);
+    }
+}
